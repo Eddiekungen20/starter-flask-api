3c7504ac0d97e3bf9e7563cd7c3225c65dd9dc9d
Eddiekungen20 <72666496+Eddiekungen20@users.noreply.github.com>
2024-02-28 22:29:03 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,21 +1,15 @@
 import sys
 from gunicorn.app.wsgiapp import run
-import json
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
 
 @app.route('/send_data', methods=['POST'])
 def send_data():
     data = request.get_json()
     print("Received data from Minecraft mod:", data)
     # Process the data as needed
-    return {'status': 'success'}
-
-
-
-
-
-
-
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     sys.argv = "gunicorn --bind 0.0.0.0:5151 app:app".split()
